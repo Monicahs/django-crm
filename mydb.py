36f1d32710f27019d7b36f5958240beb7065d47c
Monika HS <121927505+Monicahs@users.noreply.github.com>
2025-02-24 10:21:16 +0530

    Update mydb.py

--- mydb.py
+++ mydb.py
@@ -1,13 +1,1 @@
-import mysql.connector 
 
-dataBase = mysql.connector.connect(
-    host = 'localhost',
-    user ='root',
-    passwd ='Monika@123',
-)
-
-cursorObject = dataBase.cursor()
-
-cursorObject.execute("CREATE DATABASE db")
-
-print("all done")
